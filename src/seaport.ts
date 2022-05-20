cae7aafc95d8501f1aa0a58e713555a16858441c
Davydenkov Mihail <davydenkov.mihail@gmail.com>
2022-05-20 04:47:03 +0300

    return from atomicMatchAlt ABI encoded data and txnData

--- src/seaport.ts
+++ src/seaport.ts
@@ -4223,10 +4223,10 @@ export class OpenSeaPort {
       );
     }
 
-    // Then do the transaction
+    // Estimate gas first
     try {
-      this.logger(`Fulfilling order with gas set to ${txnData.gas}`);
-      txHash = await this._wyvernProtocol.wyvernExchange
+      // Typescript splat doesn't typecheck
+      const gasEstimate = await this._wyvernProtocolReadOnly.wyvernExchange
         .atomicMatch_(
           args[0],
           args[1],
@@ -4240,27 +4240,94 @@ export class OpenSeaPort {
           args[9],
           args[10]
         )
-        .sendTransactionAsync(txnData);
+        .estimateGasAsync(txnData);
+
+      txnData.gas = this._correctGasAmount(gasEstimate);
     } catch (error) {
-      console.error(error);
-
-      this._dispatch(EventType.TransactionDenied, {
-        error,
-        buy,
-        sell,
-        accountAddress,
-        matchMetadata: metadata,
-      });
-
+      console.error(`Failed atomic match with args: `, args, error);
       throw new Error(
-        `Failed to authorize transaction: "${
-          error instanceof Error && error.message
-            ? error.message
-            : "user denied"
+        `Oops, the Ethereum network rejected this transaction :( The OpenSea devs have been alerted, but this problem is typically due an item being locked or untransferrable. The exact error was "${
+          error instanceof Error
+            ? error.message.substr(0, MAX_ERROR_LENGTH)
+            : "unknown"
         }..."`
       );
     }
-    return txHash;
+
+    // Return apiEncoded data
+    let encoded;
+    try {
+      // Typescript splat doesn't typecheck
+      encoded = await this._wyvernProtocolReadOnly.wyvernExchange
+        .atomicMatch_(
+          args[0],
+          args[1],
+          args[2],
+          args[3],
+          args[4],
+          args[5],
+          args[6],
+          args[7],
+          args[8],
+          args[9],
+          args[10]
+        )
+        .getABIEncodedTransactionData();
+    } catch (error) {
+      console.error(`Failed atomic match with args: `, args, error);
+      throw new Error(
+        `Oops, the Ethereum network rejected this transaction :( The OpenSea devs have been alerted, but this problem is typically due an item being locked or untransferrable. The exact error was "${
+          error instanceof Error
+            ? error.message.substr(0, MAX_ERROR_LENGTH)
+            : "unknown"
+        }..."`
+      );
+    }
+
+    return {
+      encoded,
+      txnData,
+      args,
+    };
+
+    // // Then do the transaction
+    // try {
+    //   this.logger(`Fulfilling order with gas set to ${txnData.gas}`);
+    //   txHash = await this._wyvernProtocol.wyvernExchange
+    //     .atomicMatch_(
+    //       args[0],
+    //       args[1],
+    //       args[2],
+    //       args[3],
+    //       args[4],
+    //       args[5],
+    //       args[6],
+    //       args[7],
+    //       args[8],
+    //       args[9],
+    //       args[10]
+    //     )
+    //     .sendTransactionAsync(txnData);
+    // } catch (error) {
+    //   console.error(error);
+
+    //   this._dispatch(EventType.TransactionDenied, {
+    //     error,
+    //     buy,
+    //     sell,
+    //     accountAddress,
+    //     matchMetadata: metadata,
+    //   });
+
+    //   throw new Error(
+    //     `Failed to authorize transaction: "${
+    //       error instanceof Error && error.message
+    //         ? error.message
+    //         : "user denied"
+    //     }..."`
+    //   );
+    // }
+    // return txHash;
   }
 
   private async _atomicMatchAlt({
